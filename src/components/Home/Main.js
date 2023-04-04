45062e3132f2f54e81cc24ddc16a242d2b83baae
geekaryan <aryan.rana2021@vitbhopal.ac.in>
2023-04-04 11:16:48 +0530

    working on home page

--- src/components/Home/Main.js
+++ src/components/Home/Main.js
@@ -25,10 +25,28 @@ const Main = () => {
     <div className={styles.container}>
       <div>
         <div className={styles.text}>
-          <span>THE TASTE YOU NEED THE TASTE YOU GET.</span>
+          <span>
+            <span style={{ color: "#E50E00" }}>No</span> cooking,
+          </span>
         </div>
         <div className={styles.text}>
-          <span>ONLY HERE!</span>
+          <span>
+            No <span style={{ color: "#E50E00" }}>hassle,</span>
+          </span>
+        </div>
+        <div className={styles.text}>
+          <span>
+            just{" "}
+            <span style={{ color: "#EBC80F", textDecoration: "underline" }}>
+              order.
+            </span>
+          </span>
+        </div>
+        <div className={styles.longtext}>
+          <span>
+            Best cooks and best delivery guys all at you service. Hot tasty food
+            will reach you in 60 minutes..
+          </span>
         </div>
         <div>
           <Link to="/menu" style={{ textDecoration: "none" }}>
