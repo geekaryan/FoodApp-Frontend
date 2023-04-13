cd4135ba6f50ba607eb14d6467fc011618e36b47
geekaryan <aryan.rana2021@vitbhopal.ac.in>
2023-04-13 11:20:15 +0530

    making menu page

--- src/components/Menu/Index.js
+++ src/components/Menu/Index.js
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
+import burger from "./../../assets/burger.png";
 
-import Item from "./Item";
+// import Item from "./Item";
 import styles from "./Index.module.css";
 
 const Index = ({ data }) => {
+  console.log(data);
   return (
     <div>
       <div className={styles.srcDiv}>
@@ -13,12 +15,48 @@ const Index = ({ data }) => {
           className={styles.searchinput}
         />
       </div>
+      <div className={styles.menubar}>
+        <div>
+          <span>Main Course</span>
+        </div>
+        <div>
+          <span>Main Course</span>
+        </div>
+        <div>
+          <span>Main Course</span>
+        </div>
+        <div>
+          <span>Main Course</span>
+        </div>
+        <div>
+          <span>Main Course</span>
+        </div>
+        <div>
+          <span>Main Course</span>
+        </div>
+      </div>
       <div className={styles.apiContainer}>
         {data.map((menu) => {
           return (
             <div>
               <Link to={menu._id}>
-                <div>{menu.name}</div>
+                <div className={styles.eachitem}>
+                  <div>
+                    <img src={burger} alt="burger" />
+                  </div>
+                  <div>{menu.name}</div>
+                  <div className={styles.priceFlex}>
+                    <div>{menu.price}</div>
+                    <div className={styles.buttonflex}>
+                      <div>
+                        <button>-</button>
+                      </div>
+                      <div>
+                        <button>+</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </Link>
             </div>
           );
