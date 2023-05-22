30bc5178f19853fb4eb110c5f3e2b6ad40f9167a
geekaryan <aryan.rana2021@vitbhopal.ac.in>
2023-05-23 00:09:02 +0530

    created store

---
+++ src/store/index.js
@@ -0,0 +1,22 @@
+import { createSlice, configureStore } from "@reduxjs/toolkit";
+
+const intitalState = { count: 0 };
+
+const orderSlice = createSlice({
+  name: "order",
+  intitalState,
+  reducers: {
+    increment(state) {
+      state.count++;
+    },
+    decrement(state) {
+      state.count--;
+    },
+  },
+});
+
+const store = configureStore({
+  reducer: orderSlice.reducer,
+});
+
+export default store;
