9c5146c78dd5f53d5d2b410e2fbdc663f0dc763f
geekaryan <aryan.rana2021@vitbhopal.ac.in>
2023-06-07 12:28:54 +0530

    counter left and also post request check left in mobile version

--- src/components/Menu/MenuDetail/MobileVersion/Detail.js
+++ src/components/Menu/MenuDetail/MobileVersion/Detail.js
@@ -1,13 +1,45 @@
+// import { useState } from "react";
+import { useCookies } from "react-cookie";
+
 import burger from "./../../../../assets/mobileburgerimage.png";
 import styles from "./Detail.module.css";
 
 const Detail = ({ data }) => {
-  console.log(data);
+  // console.log(data);
+
+  const [cookies] = useCookies(["jwt", "name"]);
+  console.log(cookies.jwt);
+
+  const buyHandler = async () => {
+    try {
+      const response = await fetch("", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${cookies.jwt}`,
+        },
+        body: JSON.stringify({
+          itemName: data.name,
+          itemPrice: data.price,
+          itemQuantity: 2,
+        }),
+      });
+
+      if (!response.ok) {
+        console.log("API is not working properly");
+      }
+
+      const result = response.json();
+      console.log(result);
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <div>
       <div className={styles.containerflex}>
         <div>
-          <img src={burger} alt="burger image" />
+          <img src={burger} alt="burger" />
         </div>
         <div className={styles.flex}>
           <div>
@@ -27,6 +59,17 @@ const Detail = ({ data }) => {
         <div className={styles.description}>
           <span>{data.description}</span>
         </div>
+      </div>
+      {/* <div>
+
+      </div> */}
+      <div className="flex justify-center mt-[22px]">
+        <button
+          onClick={buyHandler}
+          className="border-solid border-2 border-black pt-[12px] pb-[12px] px-[32px] py-[32px] bg-yellow-300 hover:bg-white font-custom"
+        >
+          Buy Now
+        </button>
       </div>
     </div>
   );
