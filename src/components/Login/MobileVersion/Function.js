e7cc4f130fb511ad1f7c74c8ea6f40e229f899ea
geekaryan <aryan.rana2021@vitbhopal.ac.in>
2023-06-11 10:48:22 +0530

    added the try catch and some state managment in singup functionality

--- src/components/Login/MobileVersion/Function.js
+++ src/components/Login/MobileVersion/Function.js
@@ -48,20 +48,29 @@ const Function = () => {
   const [spassword, setSPassword] = useState("");
   const [spasswordConfirm, setSPasswordConfirm] = useState("");
   const SingupButtonHandler = async () => {
-    const response = await fetch("/api/user/signup", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        name: sname,
-        email: semail,
-        password: spassword,
-        passwordConfirm: spasswordConfirm,
-      }),
-    });
-    const data = await response.json();
-    console.log(data);
+    try {
+      const response = await fetch("/api/user/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: sname,
+          email: semail,
+          password: spassword,
+          passwordConfirm: spasswordConfirm,
+        }),
+      });
+      const data = await response.json();
+      console.log(data);
+    } catch (e) {
+      console.log(e.message);
+    }
+
+    setSName("");
+    setSEmail("");
+    setSPasswordConfirm("");
+    setSPassword("");
   };
   return (
     <div className={styles.container}>
