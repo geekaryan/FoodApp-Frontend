c3ff0416104c7d0c4456d7a3c28d131b0fe5d2c2
geekaryan <aryan.rana2021@vitbhopal.ac.in>
2023-06-13 23:53:06 +0530

    maked add item to cart function in reduceer

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -17,6 +17,23 @@ const counterSlice = createSlice({
         state.counter = 0;
       }
     },
+    addItemToCart() {
+      const newItem = action.payload;
+      const existingItem = state.items.find((item) => item._id === newItem._id);
+      if (!existingItem) {
+        state.items.push({
+          itemId: newItem._id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          name: newItem.name,
+        });
+      } else {
+        existingItem.quantity++;
+        existingItem.totalPrice = totalPrice + newItem.price;
+      }
+    },
+    removeItemFromCart() {},
   },
 });
 
