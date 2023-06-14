ef784a8fa18435def81a47b032bca6f097ca8160
geekaryan <aryan.rana2021@vitbhopal.ac.in>
2023-06-14 11:58:26 +0530

    done with the setting props

--- src/components/Menu/MenuDetail/Index.js
+++ src/components/Menu/MenuDetail/Index.js
@@ -5,12 +5,20 @@ import Feedback from "./Feedback";
 const Index = (props) => {
   //going to work with props soon as we are going to map accordingly to the redux
   //   const params = useParams();
+  console.log(props);
+  console.log(props.data._id);
   return (
     <div>
       {/* {params.id} */}
       {/* {data.name} */}
       <div>
-        <Detail />
+        <Detail
+          id={props.data._id}
+          price={props.data.price}
+          title={props.data.name}
+          description={props.data.description}
+          rating={props.data.ratings}
+        />
         {/* adding items here */}
       </div>
       <div>
