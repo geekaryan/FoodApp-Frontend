f200a22be73430e22bc7d5b4e39073dd715fab03
geekaryan <aryan.rana2021@vitbhopal.ac.in>
2023-06-14 11:25:30 +0530

    added remove cart

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -33,7 +33,16 @@ const counterSlice = createSlice({
         existingItem.totalPrice = existingItem.totalPrice + newItem.price;
       }
     },
-    removeItemFromCart() {}, //remove cart left..
+    removeItemFromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+      }
+    }, //remove cart
   },
 });
 
