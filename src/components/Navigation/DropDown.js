9ec3b0226416bac768088e84595d9150a6fb666e
geekaryan <aryan.rana2021@vitbhopal.ac.in>
2024-11-30 22:41:07 +0530

    user logged name must be shown

--- src/components/Navigation/DropDown.js
+++ src/components/Navigation/DropDown.js
@@ -2,10 +2,12 @@ import styles from "./Dropdown.module.css";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { uiActions } from "../../store/ui-slice";
+import { useSelector } from "react-redux";
 
 const DropDown = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const name = useSelector((state) => state.user.name);
 
   const showCartHandler = () => {
     dispatch(uiActions.toggle());
@@ -16,6 +18,7 @@ const DropDown = () => {
   };
   return (
     <div className={styles.mainDiv}>
+      <div className="text-[16px]">{name.length > 0 ? name : ""}</div>
       <div className={styles.txt} onClick={loginNavigation}>
         <p>Login</p>
       </div>
